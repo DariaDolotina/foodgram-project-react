571a57569dcfa8c0ca961a932bfc4955db6647e5
Daria <hwda@yandex.ru>
2021-10-10 19:57:48 +0300

    test

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -130,7 +130,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
         for ingredient in ingredients:
             IngredientAmount.objects.create(
                 recipe=recipe,
-                ingredient_id=ingredient.get('pk'),
+                ingredient_id=ingredient.get('id'),
                 amount=ingredient.get('amount'),
             )
     def create(self, validated_data):
