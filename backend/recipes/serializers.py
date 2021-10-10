03941d20595d0bb7cd2eb5ef3c36660e1cf1ee7f
Daria <hwda@yandex.ru>
2021-10-10 15:57:06 +0300

    changes

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -273,6 +273,18 @@ class FavoritesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
         fields = '__all__'
+    
+    def validate(self, data):
+        user = data['user']
+        recipe_id = data['recipe'].id
+        if Favorite.objects.filter(user=user, recipe__id=recipe_id).exists():
+            raise ValidationError('Рецепт уже добавлен в избранное')
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeReadSerializer(instance.recipe, context=context).data
 
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
